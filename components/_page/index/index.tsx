bc055bbb3ab836faee81ebfb7619a7e1796fd991
yiyenene <goodbyeboredworld@aol.jp>
2020-06-25 19:46:33 +0900

    hello world

---
+++ components/_page/index/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const IndexPage: React.FC = props => {
+    return (
+        <h1>Hello World</h1>
+    );
+}
+
+export { IndexPage };
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+import { IndexPage } from "@root/components/_page/index";
+
+const Index: NextPage<{}> = props => {
+    return (
+        <IndexPage />
+    );
+};
+
+export default Index;
